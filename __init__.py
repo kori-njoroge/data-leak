f1bf5ebf57dcda9316c4f4b9401e07fd33fa17dc
kori njoroge <korinjoroge63@gmail.com>
2023-05-12 12:09:18 +0300

    db configuration

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .config import db_string
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:MyPass@localhost:3306/dataleakDB"
+    app.config['SECRET_KEY']= os.environ.get('SECRET_KEY')
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)

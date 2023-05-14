b1578716280d0c9f4c79fe3c9342a610dc6648f7
kori njoroge <korinjoroge63@gmail.com>
2023-05-14 08:07:23 +0300

    anomaly detection route

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, url_for, request, redirect
+from flask import Blueprint, render_template, url_for, request, redirect,flash
+from .anomalies import detect_anomaly
+import pandas as pd
 
 main = Blueprint('main', __name__)
 
@@ -6,9 +8,18 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('base.html')
 
-# @main.route('/home')
-# def home():
-#     return 'home'
+@main.route('/dashboard')
+def dashboard():
+    return 'home'
+
+@main.route('/detect-anomaly', methods=['POST','GET'])
+def run_anomaly_detection():
+    # data = request.json
+    data = pd.read_csv('/home/corey/Documents/data-leak/logs.csv', parse_dates=['_time'])
+    df = pd.DataFrame(data)
+    detect_anomaly(df)
+    return 'Anomaly detection completed'
+
 
 if __name__ == '__main__':
     main.run(debug=True)

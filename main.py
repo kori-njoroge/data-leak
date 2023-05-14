cdc7707b58b175a190c1a3bc5d83d45e62a3065e
kori njoroge <korinjoroge63@gmail.com>
2023-05-14 22:20:32 +0300

    show graph route

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template, url_for, request, redirect
-from .anomalies import detect_anomaly
+from .anomalies import detect_anomaly,detect_anomaly_graph
 import pandas as pd
+import csv
 
 main = Blueprint('main', __name__)
+
+rows = [
+        {"time": "2015-05-01T00:00:00.000+03:00", "count": 4},
+        {"time": "2015-06-01T00:00:00.000+03:00", "count": 0},
+        {"time": "2015-07-01T00:00:00.000+03:00", "count": 0},
+        ]
 
 @main.route('/')
 def home():
@@ -10,16 +17,29 @@ def home():
 
 @main.route('/dashboard')
 def dashboard():
-    return render_template('home.html')
+    with open('/home/corey/Documents/data-leak/logs.csv','r') as f:
+        reader = csv.DictReader(f,delimiter=';')
+        # rows =[row for row in reader]
+        rows = [
+        {"time": "2015-05-01T00:00:00.000+03:00", "count": 4},
+        {"time": "2015-06-01T00:00:00.000+03:00", "count": 0},
+        {"time": "2015-07-01T00:00:00.000+03:00", "count": 0},
+        ]
+    return render_template('home.html',rows=rows)
 
 @main.route('/detect-anomaly', methods=['POST','GET'])
 def run_anomaly_detection():
-    # data = request.json
     data = pd.read_csv('/home/corey/Documents/data-leak/logs.csv', parse_dates=['_time'])
     df = pd.DataFrame(data)
     detect_anomaly(df)
-    return 'Anomaly detection completed'
+    return render_template('home.html',rows=rows)
 
+@main.route('/graph')
+def show_graph():
+    data = pd.read_csv('/home/corey/Documents/data-leak/logs.csv', parse_dates=['_time'])
+    df = pd.DataFrame(data)
+    detect_anomaly_graph(data)
+    return render_template('home.html', rows = rows)
 
 if __name__ == '__main__':
     main.run(debug=True)
